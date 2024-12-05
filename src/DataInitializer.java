320e7977ae98d4a0bd182a194217da91c0a3bb87
mikmself <mikmself@gmail.com>
2024-12-05 14:35:18 +0700

    fix : change matkul

--- src/DataInitializer.java
+++ src/DataInitializer.java
@@ -21,22 +21,31 @@ public class DataInitializer {
         for (int i = 1; i <= 10; i++) {
             ruangs.add(new Ruang("Ruang " + i, jenisRuang[i % jenisRuang.length], 30 + new Random().nextInt(20), true, false));
         }
-
-        // Perbanyak jumlah mata kuliah menjadi 20
         String[] matkulNames = {
-                "Matematika", "Fisika", "Kimia", "Biologi", "Ekonomi",
-                "Sosiologi", "Antropologi", "Sejarah", "Geografi", "Psikologi",
-                "Ilmu Komunikasi", "Statistika", "Pemrograman", "Jaringan Komputer", "Kalkulus",
-                "Algoritma", "Sistem Informasi", "Kewirausahaan", "Pemasaran", "Teknik Mesin"
+                "Pendidikan Pancasila dan Kewarganegaraan", "Pendidikan Agama Islam", "Sikap Mental Amikom",
+                "Kalkulus Dasar", "Sistem Basis Data", "Arsitektur dan Organisasi Komputer", "Technopreneurship",
+                "Pendidikan Anti Korupsi", "Bahasa Indonesia", "Pengantar Ilmu komputer", "Pengantar Multimedia",
+                "Pengantar Sistem Cerdas", "Pemrograman Berorientasi Objek", "Aljabar Linier dan Matrik",
+                "Matematika Diskret", "Algoritma dan Struktur Data", "Bahasa Inggris", "Bahasa Pemrograman Python",
+                "Sistem Operasi", "Jaringan Komputer", "Kalkulus Lanjut", "Statistik Probabilitas", "Pemodelan 2 Dimensi",
+                "Pembelajaran Mesin", "Logika Digital dan Sistem Digital", "Bahasa Inggris Lanjut", "Mikroprosessor",
+                "Teori Graf dan Otomata", "Pemrograman Logik dan Semantik", "Cloud Computing", "Pemrograman Mobile"
         };
-        int[] sksArray = {2, 3};
+        String[] matkulCodes = {
+                "NSIFW001", "NSIFW002", "USIFW001", "PSIFW001", "PSIFW003", "PSIFW004", "USIFW002", "USIFW003", "NSIFW008",
+                "FSIFW001", "USIFW004", "PSIFW005", "PSIFW006", "FSIFW002", "FSIFW003", "PSIFW008", "USIFW005", "PSIFW009",
+                "FSIFW004", "PSIFW010", "PSIFW002", "PSIFW012", "PSIFW013", "PSIFW031", "PSIFW014", "FSIFW005", "PSIFW020",
+                "PSIFW021", "PSIFW024", "PSIFW022", "PSIFW027"
+        };
+        int[] sksArray = {
+                3, 3, 1, 2, 4, 3, 2, 2, 3, 3, 3, 2, 3, 3, 3, 3, 2, 4, 3, 3, 2, 3, 3, 3, 2, 3, 3, 3, 2, 4
+        };
 
         for (int i = 0; i < matkulNames.length; i++) {
-            int sks = sksArray[new Random().nextInt(sksArray.length)];
+            int sks = sksArray[i];
             String jenis = (sks == 2) ? "teori" : "teori & praktek";
-            String kode = "MK" + (100 + i);
             String semester = "Semester " + (1 + (i % 8));
-            matkuls.add(new MataKuliah(matkulNames[i], sks, jenis, kode, semester));
+            matkuls.add(new MataKuliah(matkulNames[i], sks, jenis, matkulCodes[i], semester));
         }
     }
 }
