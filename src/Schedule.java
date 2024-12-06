648e6a8914a255ad321a40ba378992a556212973
mikmself <mikmself@gmail.com>
2024-12-06 16:51:32 +0700

    feat : add documentation for Schedule

--- src/Schedule.java
+++ src/Schedule.java
@@ -1,10 +1,31 @@
+/**
+ * Kelas Schedule merepresentasikan sebuah jadwal dengan atribut-atribut dasar seperti mata pelajaran, waktu, hari, dosen, dan ruangan.
+ */
 class Schedule {
+    // Mata pelajaran
     String subject;
+
+    // Waktu
     String time;
+
+    // Hari
     String day;
+
+    // Dosen
     String lecturer;
+
+    // Ruangan
     String room;
 
+    /**
+     * Konstruktor untuk membuat objek Schedule baru.
+     *
+     * @param subject Mata pelajaran
+     * @param time Waktu
+     * @param day Hari
+     * @param lecturer Dosen
+     * @param room Ruangan
+     */
     public Schedule(String subject, String time, String day, String lecturer, String room) {
         this.subject = subject;
         this.time = time;
