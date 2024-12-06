f1dbfdf8e9f22a4a2e27ee02cb0add11257ac88f
mikmself <mikmself@gmail.com>
2024-12-06 16:57:47 +0700

    feat : add documentation for Scheduling

--- src/Scheduling.java
+++ src/Scheduling.java
@@ -1,17 +1,30 @@
 import java.util.Scanner;
 
+/**
+ * Kelas `Scheduling` adalah kelas utama yang menginisialisasi data, memungkinkan pengguna untuk memilih mata kuliah,
+ * dan menjalankan algoritma untuk menghasilkan jadwal.
+ */
 public class Scheduling {
     public static void main(String[] args) {
+        // Menginisialisasi data dummy
         DataInitializer.createDummyData();
+
+        // Memilih mata kuliah
         CourseSelector.selectCourses();
+
+        // Meminta input dari pengguna untuk berapa kali menjalankan algoritma
         System.out.print("input how many times you want to run the algorithm : ");
         Scanner scanner = new Scanner(System.in);
         int times = scanner.nextInt();
+
+        // Menjalankan algoritma sebanyak input pengguna
         for (int i = 0; i < times; i++) {
             SchedulePrinter.generateAndPrintSchedule("Genetic Algorithm", ScheduleGenerator::geneticAlgorithm);
             SchedulePrinter.generateAndPrintSchedule("Simulated Annealing", ScheduleGenerator::simulatedAnnealing);
             SchedulePrinter.generateAndPrintSchedule("Tabu Search", ScheduleGenerator::tabuSearch);
         }
+
+        // Mencetak tabel waktu eksekusi
         SchedulePrinter.printTableOfExecutionTimes();
     }
 }
