fe35def6b6269e48feb732b1af74ec24168b1f49
mikmself <mikmself@gmail.com>
2024-12-06 16:50:26 +0700

    feat : add documentation for Lecturer

--- src/Lecturer.java
+++ src/Lecturer.java
@@ -1,7 +1,19 @@
+/**
+ * Kelas Lecturer merepresentasikan seorang dosen dengan atribut-atribut dasar seperti nama dan NIP.
+ */
 class Lecturer {
+    // Nama dosen
     String name;
+
+    // Nomor Induk Pegawai (NIP) dosen
     String nip;
 
+    /**
+     * Konstruktor untuk membuat objek Lecturer baru.
+     *
+     * @param name Nama dosen
+     * @param nip Nomor Induk Pegawai (NIP) dosen
+     */
     public Lecturer(String name, String nip) {
         this.name = name;
         this.nip = nip;
