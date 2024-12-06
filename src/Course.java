c219644100dd833c6f1d19ec748dd95ae687081d
mikmself <mikmself@gmail.com>
2024-12-06 16:48:34 +0700

    feat : add documentation for Course

--- src/Course.java
+++ src/Course.java
@@ -1,10 +1,31 @@
+/**
+ * Kelas Course merepresentasikan sebuah mata kuliah dengan atribut-atribut dasar seperti nama, jumlah kredit, tipe, kode, dan semester.
+ */
 class Course {
+    // Nama mata kuliah
     String name;
+
+    // Jumlah kredit mata kuliah
     int credits;
+
+    // Tipe mata kuliah (misalnya: wajib, pilihan)
     String type;
+
+    // Kode mata kuliah
     String code;
+
+    // Semester di mana mata kuliah ditawarkan
     String semester;
 
+    /**
+     * Konstruktor untuk membuat objek Course baru.
+     *
+     * @param name Nama mata kuliah
+     * @param credits Jumlah kredit mata kuliah
+     * @param type Tipe mata kuliah (misalnya: wajib, pilihan)
+     * @param code Kode mata kuliah
+     * @param semester Semester di mana mata kuliah ditawarkan
+     */
     public Course(String name, int credits, String type, String code, String semester) {
         this.name = name;
         this.credits = credits;
